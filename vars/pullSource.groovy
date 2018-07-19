b495227a4bffa397c979864b53de9d9854891c88
Matt Hicks <mhicks@cloudbees.com>
2018-07-19 15:10:52 -0400

    Use println not echo

--- vars/pullSource.groovy
+++ vars/pullSource.groovy
@@ -1,6 +1,6 @@
 import static groovy.json.JsonOutput.*
 
 def call(Map args = [:], Closure body = {}) {
-    echo "${env.STAGE_NAME} called with: " + prettyPrint(toJson(args))
-    echo pullSource.dump()
+    println "${env.STAGE_NAME} called with: " + prettyPrint(toJson(args))
+    println pullSource.dump()
 }
