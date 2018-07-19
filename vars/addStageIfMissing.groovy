7a07b30ded9a42c86c50028c4951d0ac908005aa
Matt Hicks <mhicks@cloudbees.com>
2018-07-19 14:22:50 -0400

    Always add stage

--- vars/addStageIfMissing.groovy
+++ vars/addStageIfMissing.groovy
@@ -2,16 +2,9 @@ import static groovy.json.JsonOutput.*
 
 def call(Map args) {
 
-    self.dump
-
-    // If we are not in the correct stage, wrap with stage
-    if(params.STAGE_NAME != "A. Pull Source") {
-        stage("A. Pull Source") {
-            steps.pullSource(args)
-            return
-        }
+    stage("A. Pull Source") {
+        echo "${params.STAGE_NAME} called with:"
+        echo prettyPrint(toJson(args))
     }
 
-    echo "${params.STAGE_NAME} called with:"
-    echo prettyPrint(toJson(args))
 }
