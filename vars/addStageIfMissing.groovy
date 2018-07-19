e952cd489e5c42c2738c7aae54d730ba71a4dc65
Matt Hicks <mhicks@cloudbees.com>
2018-07-19 14:19:46 -0400

    Look for a reference to self

--- vars/addStageIfMissing.groovy
+++ vars/addStageIfMissing.groovy
@@ -2,6 +2,8 @@ import static groovy.json.JsonOutput.*
 
 def call(Map args) {
 
+    self.dump
+
     // If we are not in the correct stage, wrap with stage
     if(params.STAGE_NAME != "A. Pull Source") {
         stage("A. Pull Source") {
