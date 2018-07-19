caa1fe2b33a4804a225c53825870b1ad05f3d100
Matt Hicks <mhicks@cloudbees.com>
2018-07-19 14:16:54 -0400

    Reference step with steps.step in definition

--- vars/addStageIfMissing.groovy
+++ vars/addStageIfMissing.groovy
@@ -5,11 +5,11 @@ def call(Map args) {
     // If we are not in the correct stage, wrap with stage
     if(params.STAGE_NAME != "A. Pull Source") {
         stage("A. Pull Source") {
-            pullSource(args)
+            steps.pullSource(args)
             return
         }
     }
-    
+
     echo "${params.STAGE_NAME} called with:"
     echo prettyPrint(toJson(args))
 }
