99fb9f16f9022af2f93cccf2b2b668dfe0a80a00
Matt Hicks <mhicks@cloudbees.com>
2018-10-15 17:50:16 -0400

    Add mocked junit output to unitTests step

--- vars/unitTests.groovy
+++ vars/unitTests.groovy
@@ -2,4 +2,5 @@ import static groovy.json.JsonOutput.*
 
 def call(Map args = [:], Closure body = {}) {
     println "unitTests called with: " + prettyPrint(toJson(args))
+    steps.sh "mkdir tests || true; curl -o tests/log.xml https://gist.githubusercontent.com/n1k0/4332371/raw/14fdb095d43062da2a711e641b7b5e01ce455a32/log.xml"
 }
