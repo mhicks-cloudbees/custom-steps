20035b0b8e89b185b276cd919e874b2661e94f04
Matt Hicks <mhicks@cloudbees.com>
2018-12-01 15:39:29 -0500

    Add library call for testing

--- vars/integrationTests.groovy
+++ vars/integrationTests.groovy
@@ -1,8 +1,6 @@
 import static groovy.json.JsonOutput.*
 
-library 'agents'
-
 def call(Map args = [:], Closure body = {}) {
     println "integrationTests called with: " + prettyPrint(toJson(args))
-    addAgent('intTestAgent', '192.168.1.12', 'nodekey')
+    library('agents').addAgent('intTestAgent', '192.168.1.12', 'nodekey')
 }
